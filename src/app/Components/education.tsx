1bc56c6623a46d045566ee4dc148359ce5dc1844
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-16 16:04:29 -0300

    Added text highlight on hover

--- src/app/Components/education.tsx
+++ src/app/Components/education.tsx
@@ -11,12 +11,12 @@ export default function Education() {
           Formação
         </h2>
         <ul>
-        <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all hover:bg-white/10 hover:shadow">
+        <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all group hover:bg-white/10 hover:shadow">
               <h4 className="text-title w-auto lg:text-m sm:text-sm font-noto ml-2 mr-5 row-span-3 text-left">
                 2019 - hoje
               </h4>
               <div className="ml-2 grid grid-cols-[1fr-2fr]">
-                <h3 className="text-text font-montserrat text-m">
+                <h3 className="text-text font-montserrat text-m group-hover:text-highlight">
                   Bacharelado em Engenharia Química
                 </h3>
                 <Link href='/' className='hidden items-center ml-3'>
@@ -25,12 +25,12 @@ export default function Education() {
                 <p className="mt-2 mb-5 col-span-2 font-montserrat text-title">Universidade Estadual de Maringá</p>
               </div>
             </li>
-            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all hover:bg-white/10 hover:shadow">
+            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all group hover:bg-white/10 hover:shadow">
               <h4 className="text-title w-auto lg:text-m sm:text-sm font-noto ml-2 mr-20 row-span-3 text-left">
                 2023
               </h4>
               <div className="ml-2 grid grid-cols-[1fr-2fr]">
-                <h3 className="text-text font-montserrat text-m">
+                <h3 className="text-text font-montserrat text-m group-hover:text-highlight">
                   Curso de Programação em C#
                 </h3>
                 <Link href='https://www.udemy.com/certificate/UC-bed73e22-a2a7-4b72-9f82-894fe9d148cb/' target='blank' className='flex items-center ml-3'>
@@ -50,12 +50,12 @@ export default function Education() {
                 </div>
               </div>
             </li>
-            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all hover:bg-white/10 hover:shadow">
+            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all group hover:bg-white/10 hover:shadow">
               <h4 className="text-title w-auto lg:text-m sm:text-sm font-noto ml-2 mr-20 row-span-3 text-left">
                 2023
               </h4>
               <div className="ml-2 grid grid-cols-[1fr-2fr]">
-                <h3 className="text-text font-montserrat text-m">
+                <h3 className="text-text font-montserrat text-m group-hover:text-highlight">
                   Curso de Controle de Processos com Python
                 </h3>
                 <Link href='https://www.udemy.com/certificate/UC-280ea3e7-f57e-4ef3-aa85-964c097b7823/' target='blank' className='flex items-center ml-3'>
@@ -75,12 +75,12 @@ export default function Education() {
                 </div>
               </div>
             </li>
-            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all hover:bg-white/10 hover:shadow">
+            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all group hover:bg-white/10 hover:shadow">
               <h4 className="text-title w-auto lg:text-m sm:text-sm font-noto ml-2 mr-20 row-span-3 text-left">
                 hoje
               </h4>
               <div className="ml-2 grid grid-cols-[1fr-2fr]">
-                <h3 className="text-text font-montserrat text-m">
+                <h3 className="text-text font-montserrat text-m group-hover:text-highlight">
                   Curso de JavaScript e TypeScript
                 </h3>
                 <Link href='/' className='hidden items-center ml-3'>
@@ -118,12 +118,12 @@ export default function Education() {
                 </div>
               </div>
             </li>
-            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all hover:bg-white/10 hover:shadow">
+            <li className="appear rounded-lg flex lg:flex-row sm:flex-col align-middle mb-5 py-5 transition-all group hover:bg-white/10 hover:shadow">
               <h4 className="text-title w-auto lg:text-m sm:text-sm font-noto ml-2 mr-20 row-span-3 text-left">
                 hoje
               </h4>
               <div className="ml-2 grid grid-cols-[1fr-2fr]">
-                <h3 className="text-text font-montserrat text-m">
+                <h3 className="text-text font-montserrat text-m group-hover:text-highlight">
                   Aspen Plus V11 Masterclass
                 </h3>
                 <Link href='/' className='hidden items-center ml-3'>
