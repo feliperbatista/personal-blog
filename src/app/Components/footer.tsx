3d5b18ac3cd706067a685624975947f0adae2697
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-16 14:18:19 -0300

    Fixed footer

--- src/app/Components/footer.tsx
+++ src/app/Components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-8 ml-10 lg:mr-32 sm:mr-10 mb-2 text-center">
-        <p className="text-title text-xs">Template inspriado em Brittany Chiang</p>
+        <p className="text-title text-xs">Template inspirado em Brittany Chiang</p>
       </div>
     </footer>
   );
