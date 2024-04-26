54a2dd33bc5c3256116f6c76fe219f9f50e81f40
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-26 13:37:10 -0300

    Fixed media queries

--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -4,9 +4,9 @@ import { FaLinkedin, FaInstagram, FaGithub, FaGoodreads } from 'react-icons/fa';
 export default function Header() {
 
   return (
-    <header className="lg:h-screen sm:h-50 flex justify-center items-center flex-col row-span-4 lg:sticky top-0">
+    <header className="md:h-screen md:mt-0 lg:mt-0 sm:mt-28 flex justify-center items-center flex-col row-span-4 lg:sticky md:sticky top-0">
       <div className="px-10">
-        <div className="lg:flex md:hidden sm:hidden justify-center gap-3">
+        <div className="flex md:flex sm:hidden mb-36 justify-center gap-3">
           <Link
             href="#about"
             className="text-title font-noto transition hover:text-highlight focus:text-highlight"
@@ -28,20 +28,20 @@ export default function Header() {
             Experiência
           </Link>
         </div>
-        <h2 className="font-noto text-highlight font-bold text-lg lg:mt-56 sm:mt-20">
-          Olá! Eu sou o
-        </h2>
-        <h1 className="my-3 font-michroma text-title lg:text-3xl sm:text-2xl font-bold">
-          <Link href="/" className='font-michroma'>Felipe Rodrigues Batista</Link>
-        </h1>
-        <div className="font-montserrat text-text lg:mb-56 sm:mb-20 sm:w-44 lg:w-auto">
-          <div className='container'>
+        <div className='my-auto'>
+          <h2 className="font-noto text-highlight font-bold text-lg">
+            Olá! Eu sou o
+          </h2>
+          <h1 className="my-3 font-michroma text-title lg:text-3xl sm:text-2xl font-bold">
+            <Link href="/" className='font-michroma'>Felipe Rodrigues Batista</Link>
+          </h1>
+          <div className="font-montserrat text-text sm:w-44 lg:w-auto">
             <h3>Engenheiro químico.</h3>
             <h3>Dev em construção.</h3>
             <h3>Curioso.</h3>
           </div>
         </div>
-        <div className="flex justify-center flex-row gap-3">
+        <div className="flex justify-center flex-row gap-3 mt-36">
           <Link href="https://www.linkedin.com/in/feliperodriguesbatista/" target='blank'>
             <FaLinkedin className="h-10 w-10 fill-highlight hover:fill-highlightdarker"></FaLinkedin>
           </Link>
